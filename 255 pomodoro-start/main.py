aa949e4cf0f1a1d24ca3b6f9da74000de1bff180
Joshua Ogundairo <oluwadamilarejoshua01@gmail.com>
2022-10-20 14:03:58 +0100

    Pomodoro timmer app commenced

--- 255 pomodoro-start/main.py
+++ 255 pomodoro-start/main.py
@@ -17,9 +17,13 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-window =Tk()
+window = Tk()
 window.title('Pomodoro')
 
+canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=tomato_img)
+canvas.pack()
 
 
 window.mainloop()
